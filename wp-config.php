458676aceded87911412cf5c5553619e48f10ac2
Kevin Campbell <khjc2012@gmail.com>
2022-01-11 23:57:08 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'heroku_7efc49c06adf3a3' );
+define( 'DB_NAME', 'measuringStick' );
 
 /** Database username */
-define( 'DB_USER', 'b57a6bbd298b8f' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '20c89ec1' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'us-cdbr-east-05.cleardb.net' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
